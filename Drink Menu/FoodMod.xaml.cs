522adb95bf84286e8de01009f2aa2dc4255d8947
Yue <yuechen1@live.com>
2017-03-02 20:37:31 -0700

    added the food mod menu layout

---
+++ Drink Menu/FoodMod.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Drink_Menu
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Food
+    {
+        public string Image1 { set; get; }
+        public string Image2 { set; get; }
+        public List<ingredient> ingredients = new List<ingredient>();
+    }
+
+    public class ingredient
+    {
+        public string name { set; get; }
+        public string price { set; get; }
+    }
+}
