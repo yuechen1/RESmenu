4f13c4da5490a18f88534ef0ff693c93f2656d1f
Yue <yuechen1@live.com>
2017-03-04 19:34:51 -0700

    output the right stuff in lists

--- Drink Menu/MainWindow.xaml.cs
+++ Drink Menu/MainWindow.xaml.cs
@@ -24,19 +24,24 @@ namespace Drink_Menu
         {
             InitializeComponent();
             List<Drinkdisplay> items = new List<Drinkdisplay>();
-            items.Add(new Drinkdisplay { D1 = new DrinkItem { Image = null, Name = "suger water", price12 = "8.00", price16 = "10.00" }, D2 = new DrinkItem { Image = null, Name = "Water", price12 = "8.00", price16 = "10.00" } });
-            items.Add(new Drinkdisplay { D1 = new DrinkItem { Image = null, Name = "suger water", price12 = "8.00", price16 = "10.00" }, D2 = new DrinkItem { Image = null, Name = "Water", price12 = "8.00", price16 = "10.00" } });
-            items.Add(new Drinkdisplay { D1 = new DrinkItem { Image = null, Name = "suger water", price12 = "8.00", price16 = "10.00" }, D2 = new DrinkItem { Image = null, Name = "Water", price12 = "8.00", price16 = "10.00" } });
-            items.Add(new Drinkdisplay { D1 = new DrinkItem { Image = null, Name = "suger water", price12 = "8.00", price16 = "10.00" }, D2 = new DrinkItem { Image = null, Name = "Water", price12 = "8.00", price16 = "10.00" } });
-            items.Add(new Drinkdisplay { D1 = new DrinkItem { Image = null, Name = "suger water", price12 = "8.00", price16 = "10.00" }, D2 = new DrinkItem { Image = null, Name = "Water", price12 = "8.00", price16 = "10.00" } });
-            items.Add(new Drinkdisplay { D1 = new DrinkItem { Image = null, Name = "suger water", price12 = "8.00", price16 = "10.00" }, D2 = new DrinkItem { Image = null, Name = "Water", price12 = "8.00", price16 = "10.00" } });
+            items.Add(new Drinkdisplay {Image = null, Name = "suger water", price12 = "8.00", price16 = "10.00", Image_1 = null, Name_1 = "Water", price12_1 = "8.00", price16_1 = "10.00" });
+            items.Add(new Drinkdisplay { Image = null, Name = "suger water", price12 = "8.00", price16 = "10.00", Image_1 = null, Name_1 = "Water", price12_1 = "8.00", price16_1 = "10.00" });
+            items.Add(new Drinkdisplay { Image = null, Name = "suger water", price12 = "8.00", price16 = "10.00", Image_1 = null, Name_1 = "Water", price12_1 = "8.00", price16_1 = "10.00" });
+            items.Add(new Drinkdisplay { Image = null, Name = "suger water", price12 = "8.00", price16 = "10.00", Image_1 = null, Name_1 = "Water", price12_1 = "8.00", price16_1 = "10.00" });
+            items.Add(new Drinkdisplay { Image = null, Name = "suger water", price12 = "8.00", price16 = "10.00", Image_1 = null, Name_1 = "Water", price12_1 = "8.00", price16_1 = "10.00" });
             DrinkBox.ItemsSource = items;
         }
 
         public class Drinkdisplay
         {
-            public DrinkItem D1 { get; set; }
-            public DrinkItem D2 { get; set; }
+            public string Image { get; set; }
+            public string Name { get; set; }
+            public string price12 { get; set; }
+            public string price16 { get; set; }
+            public string Image_1 { get; set; }
+            public string Name_1 { get; set; }
+            public string price12_1 { get; set; }
+            public string price16_1 { get; set; }
         }
 
         public class DrinkItem
